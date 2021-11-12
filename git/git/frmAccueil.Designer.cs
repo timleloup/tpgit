d1da3311cd3b2a67c0bd008694b13cdf7a92ef28
Timothee VISBECQ C1E25 <c1e25>
2021-11-12 14:07:51 +0100

    classes

--- git/git/frmAccueil.Designer.cs
+++ git/git/frmAccueil.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace git
 {
-    partial class Form1
+    partial class frmAccueil
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,29 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAccueil));
             this.label1 = new System.Windows.Forms.Label();
+            this.gestionClasses = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(220, 26);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(237, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(353, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = " Lycée Général d’Ingénierie Technique ";
             // 
-            // Form1
+            // gestionClasses
+            // 
+            this.gestionClasses.Location = new System.Drawing.Point(101, 107);
+            this.gestionClasses.Name = "gestionClasses";
+            this.gestionClasses.Size = new System.Drawing.Size(131, 59);
+            this.gestionClasses.TabIndex = 1;
+            this.gestionClasses.Text = "gestion Classes";
+            this.gestionClasses.UseVisualStyleBackColor = true;
+            this.gestionClasses.Click += new System.EventHandler(this.gestionClasses_Click);
+            // 
+            // frmAccueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gestionClasses);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "frmAccueil";
+            this.Text = "Accueil";
+            this.Load += new System.EventHandler(this.frmAccueil_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +74,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button gestionClasses;
     }
 }
 
--- git/git/frmClasses.cs
+++ git/git/frmClasses.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace git
 {
-    public partial class Form1 : Form
+    public partial class frmClasses : Form
     {
-        public Form1()
+        public frmClasses()
         {
             InitializeComponent();
         }
